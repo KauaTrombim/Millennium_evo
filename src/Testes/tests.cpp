cb08677be5b74cfa5b6dfdff01888f846de566ff
Miya <miyaparusrios@gmail.com>
2025-11-18 22:55:57 -0300

    added asteroids + more
    - added asteroids class
    - added screen bounce instead of screen wrap behavior to ships + asteroids
    - added shiny triangle class
    - fixed some jank with the way ships were rendered (would cause bugs when changing scale)
    - changed ship class member names to be less redundant
    - start tracking ship total distance moved
    - change test bg to black

--- src/Testes/tests.cpp
+++ src/Testes/tests.cpp
@@ -1,67 +1,68 @@
 #include "raylib.h"
 #include <cmath>
+#include <vector>
 #include "testship.cpp"
+#include "testasteroid.cpp"
+#include "shinytriangle.cpp"
 
 int main(void)
 {
     // init
-    const int screenWidth = 1200;
+    const int screenWidth = 1600;
     const int screenHeight = 900;
-
     InitWindow(screenWidth, screenHeight, "miya raylib playground");
-
-
     SetTargetFPS(60);
-    // triangle things
-    float posv1x = screenWidth/2, posv1y = screenHeight/2, posv2x = 100.0, posv2y = screenHeight - 100.0;
-    float posv3x = screenWidth - 100.0, posv3y = screenHeight - 100.0;
-    unsigned char a = 0,b = 0,c = 0;
 
-    //ship things
+    // triangle things
+    Shinytriangle test_triangle(screenWidth/2, screenHeight/2, 300, 100);
+    // ship things
     Ship test_ship(screenWidth/2, screenHeight/2, screenWidth, screenHeight);
+    // asteroid things
+    std::vector<Asteroid> asteroids(10);
+    for(int i=0; i<5; i++){
+        Asteroid temp = Asteroid(1.0f*GetRandomValue(0,screenWidth),1.0f*GetRandomValue(0,screenHeight),GetRandomValue(50,150),screenWidth,screenHeight);
+        asteroids[i] = temp;
+    }
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {   
-        // update triangle
-        posv1x = screenWidth/2 + 150*cosf(GetTime());
-        posv1y = screenHeight/2 + 150*sinf(GetTime());
-
-        posv2y = screenHeight - 200 - 100.0*sinf(GetTime());
-        posv3y = screenHeight - 200 - 100.0*cosf(GetTime());
-
-        a = sinf(GetTime()+2)*200;
-        b = sinf(GetTime()+3)*200;
-        c = sinf(GetTime()+4)*200;
+        //update triangle
+        test_triangle.update();
         
+        //update ship
         test_ship.scan_inputs();
         test_ship.update();
 
+        //update asteroids
+        for(int i=0; i<5; i++){
+            asteroids[i].update();
+        }
+
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
+            ClearBackground(BLACK);
 
             //draw the triangle
-            DrawTriangleLines({ posv1x , posv1y },
-                         { posv2x , posv2y },
-                         { posv3x , posv3y }, BLACK);
+            test_triangle.Draw();
 
-            DrawTriangle({ posv1x , posv1y },
-                         { posv2x , posv2y },
-                         { posv3x , posv3y }, Color{a,b,c,200});
-            
-            DrawLine(screenWidth/2, screenHeight/3,posv1x, posv1y, BLACK);
-            DrawLine(screenWidth/2, screenHeight/3,posv2x, posv2y, BLACK);
-            DrawLine(screenWidth/2, screenHeight/3,posv3x, posv3y, BLACK);
-            
+            //draw the asteroids
+            for(auto a : asteroids){
+                a.Draw();
+                a.DrawExtra();
+            }
+
+            //draw the ship
             test_ship.Draw();
             test_ship.DrawExtra();
 
             //draw info on screen
             DrawFPS(GetScreenWidth() - 95, 10);
-            DrawText(TextFormat("Facing angle: %03f", RAD2DEG*test_ship.ship_facing_angle), 10, 50, 10, GRAY);
-            DrawText(TextFormat("Ship velocity x: %03f", test_ship.shipspeeds.x), 10, 70, 10, GRAY);
-            DrawText(TextFormat("Ship velocity y: %03f", test_ship.shipspeeds.y), 10, 90, 10, GRAY);
+            DrawText(TextFormat("Absolute Speed: %03.03f", test_ship.abs_speed), 10, 10, 20, WHITE);
+            DrawText(TextFormat("Distance moved: %03.0f", test_ship.distancemoved), 10, 40, 20, WHITE);
+            DrawText(TextFormat("Ship velocity x: %03.03f", test_ship.speeds.x), 10, 70, 10, WHITE);
+            DrawText(TextFormat("Ship velocity y: %03.03f", test_ship.speeds.y), 10, 90, 10, WHITE);
+            DrawText(TextFormat("Facing angle: %03.03f", RAD2DEG*test_ship.facing_angle), 10, 110, 10, WHITE);
 
         EndDrawing();
 
--- src/Testes/testship.cpp
+++ src/Testes/testship.cpp
@@ -1,96 +1,123 @@
 #include "raylib.h"
 #include <cmath>
 
+//parametros para o comportamento da nave
+#define drag 0.99
+#define accel 0.5
+#define angaccel 0.01
+#define maxang 0.1
+#define angdrag 0.9
+#define collrad 25
+
 class Ship{
     private:
 
-        Texture ship_texture = LoadTexture("Testes/millenium.png");
-        Rectangle sourceShip = { 0, 0, ship_texture.width/1.0f, ship_texture.height/1.0f };
-        Vector2 originrec = {ship_texture.width/20.0f, ship_texture.height/20.0f};
-        int screenHeight;
-        int screenWidth;
+    Texture texture = LoadTexture("millenium.png");
+    Rectangle source = { 0, 0, texture.width/1.0f, texture.height/1.0f };
+    int screenHeight;
+    int screenWidth;
 
     public:
 
-        Vector2 shipposition;
-        float ship_speed_angle;
-        float ship_facing_angle;
-        Vector2 shipspeeds;
-        float shipangularvelocity;
-        float max_angular_velocity;
-        float abs_speed;
-        float ship_acceleration;
-        float ship_angular_acceleration;
-        //construtor
-        Ship(float x, float y, int windowWidth, int windowHeight){
-            shipposition = { x , y};
-            ship_speed_angle = 0;
-            ship_facing_angle = 0;
-            shipspeeds = { 0 , 0 };
-            max_angular_velocity = 0.1;
-            ship_acceleration = 0.5;
-            abs_speed = 0;
-            ship_angular_acceleration = 0.01;
-            screenHeight = windowHeight;
-            screenWidth = windowWidth;
-        }
-        
-        void update(){
-            //update positions
-            shipposition.x += shipspeeds.x;
-            shipposition.y += shipspeeds.y;
+    Vector2 position;
+    Vector2 speeds;
+    float speed_angle;
+    float facing_angle;
+    float angularvelocity;
+    float max_angular_velocity;
+    float abs_speed;
+    float acceleration;
+    float angular_acceleration;
+    float collisionradius;
+    float distancemoved;
+    int bounce_on_edges;
 
-            //update angular velocity and speeds
-            ship_facing_angle += shipangularvelocity;
-            shipangularvelocity *= 0.9;
-            shipspeeds = { shipspeeds.x*0.98f, shipspeeds.y*0.98f };
-            if(ship_facing_angle > 360*DEG2RAD) ship_facing_angle -= DEG2RAD*360.0;
-            if(ship_facing_angle < 0) ship_facing_angle += DEG2RAD*360.0;
-            abs_speed = sqrt(shipspeeds.x*shipspeeds.x + shipspeeds.y*shipspeeds.y);
-            ship_speed_angle = atan2f(shipspeeds.y, shipspeeds.x);
-            
-            
-
-            //screen wrap
-            if(shipposition.x > screenWidth) shipposition.x = 0;
-            if(shipposition.x < 0) shipposition.x = screenWidth;
-            if(shipposition.y > screenHeight) shipposition.y = 0;
-            if(shipposition.y < 0) shipposition.y = screenHeight;
-
-            //cap ship angular velocity
-            if(shipangularvelocity > max_angular_velocity) shipangularvelocity = max_angular_velocity;
-            if(shipangularvelocity < -max_angular_velocity) shipangularvelocity = -max_angular_velocity;
-        }
-
-        void scan_inputs(){
-            if (IsKeyDown(KEY_RIGHT)) shipangularvelocity += ship_angular_acceleration;
-            if (IsKeyDown(KEY_LEFT)) shipangularvelocity -= ship_angular_acceleration;
-            if (IsKeyDown(KEY_UP)){
-                shipspeeds.x += ship_acceleration*cosf(ship_facing_angle);
-                shipspeeds.y += ship_acceleration*sinf(ship_facing_angle);
-            }
-            if (IsKeyDown(KEY_DOWN)){
-                shipspeeds.x -= 0.5*cosf(ship_facing_angle);
-                shipspeeds.y -= 0.5*sinf(ship_facing_angle);
-            }
-        }
-
-        void Draw(){
-            Rectangle destrec = {shipposition.x, shipposition.y,ship_texture.width/10.0f,ship_texture.height/10.0f};
-            DrawTexturePro(ship_texture, sourceShip, destrec, originrec, RAD2DEG*ship_facing_angle, WHITE);
-        }
-
-        void DrawExtra(){
-            Vector2 velocity_endpoint = {shipposition.x + 10*shipspeeds.x, shipposition.y + 10*shipspeeds.y};
-            Vector2 arrowhead_1 = {velocity_endpoint.x - abs_speed*cosf(ship_speed_angle+DEG2RAD*30.0), velocity_endpoint.y - abs_speed*sinf(ship_speed_angle+DEG2RAD*30)};
-            Vector2 arrowhead_2 = {velocity_endpoint.x - abs_speed*cosf(ship_speed_angle-DEG2RAD*30.0), velocity_endpoint.y - abs_speed*sinf(ship_speed_angle-DEG2RAD*30)};
-            Vector2 arrowhead_3 = {velocity_endpoint.x, velocity_endpoint.y};
-            DrawLineV(shipposition, velocity_endpoint, RED);
-            DrawTriangle(arrowhead_1, arrowhead_2, arrowhead_3, RED);
-
-            DrawCircleSector(shipposition,250,ship_facing_angle*RAD2DEG+30,ship_facing_angle*RAD2DEG-30,15,Color{10,120,200,100});
-        }
-
+    //construtor
+    Ship() = default;
+    Ship(float x, float y, int windowWidth, int windowHeight){
+        position = { x , y };
+        distancemoved = 0;
+        speed_angle = 0;
+        facing_angle = 0;
+        speeds = { 0 , 0 };
+        max_angular_velocity = maxang;
+        acceleration = accel;
+        abs_speed = 0;
+        angular_acceleration = angaccel;
+        screenHeight = windowHeight;
+        screenWidth = windowWidth;
+        collisionradius = collrad;
+    }
     
+    void update(){
+        //update positions
+        position.x += speeds.x;
+        position.y += speeds.y;
+
+        //update angular velocity and speeds
+        facing_angle += angularvelocity;
+        angularvelocity *= angdrag;
+        speeds = { speeds.x*(float)drag, speeds.y*(float)drag };
+        if(facing_angle > 360*DEG2RAD) facing_angle -= DEG2RAD*360.0;
+        if(facing_angle < 0) facing_angle += DEG2RAD*360.0;
+        abs_speed = sqrt(speeds.x*speeds.x + speeds.y*speeds.y);
+        distancemoved += abs_speed;
+        speed_angle = atan2f(speeds.y, speeds.x);            
+
+        //screen wrap
+        /*if(position.x > screenWidth) position.x = 0;
+        if(position.x < 0) position.x = screenWidth;
+        if(position.y > screenHeight) position.y = 0;
+        if(position.y < 0) position.y = screenHeight;*/
+
+        //screen edge bounce
+        if(position.x + speeds.x >= screenWidth || position.x + speeds.x <= 0){
+            position.x -= speeds.x;
+            speeds.x *= -1;
+        }
+        if(position.y + speeds.y >= screenHeight || position.y + speeds.y <= 0){
+            position.y -= speeds.y;
+            speeds.y *= -1;
+        }
+
+        //cap ship angular velocity
+        if(angularvelocity > max_angular_velocity) angularvelocity = max_angular_velocity;
+        if(angularvelocity < -max_angular_velocity) angularvelocity = -max_angular_velocity;
+    }
+
+    void scan_inputs(){
+        if (IsKeyDown(KEY_RIGHT)) angularvelocity += angular_acceleration;
+        if (IsKeyDown(KEY_LEFT)) angularvelocity -= angular_acceleration;
+        if (IsKeyDown(KEY_UP)){
+            speeds.x += acceleration*cosf(facing_angle);
+            speeds.y += acceleration*sinf(facing_angle);
+        }
+        if (IsKeyDown(KEY_DOWN)){
+            speeds.x -= 0.5*cosf(facing_angle);
+            speeds.y -= 0.5*sinf(facing_angle);
+        }
+    }
+
+    void Draw(){
+        Rectangle dest = {position.x, position.y,collisionradius*2.5f,collisionradius*2.0f};
+        Vector2 origin = {dest.width/2.0f, dest.height/2.0f};
+        DrawTexturePro(texture, source, dest, origin, RAD2DEG*facing_angle, WHITE);
+    }
+
+    void DrawExtra(){
+        Vector2 velocity_endpoint = {position.x + 10*speeds.x, position.y + 10*speeds.y};
+        Vector2 arrowhead_1 = {velocity_endpoint.x - abs_speed*cosf(speed_angle+DEG2RAD*30.0), velocity_endpoint.y - abs_speed*sinf(speed_angle+DEG2RAD*30)};
+        Vector2 arrowhead_2 = {velocity_endpoint.x - abs_speed*cosf(speed_angle-DEG2RAD*30.0), velocity_endpoint.y - abs_speed*sinf(speed_angle-DEG2RAD*30)};
+        Vector2 arrowhead_3 = {velocity_endpoint.x, velocity_endpoint.y};
+        DrawLineV(position, velocity_endpoint, RED);
+        DrawTriangle(arrowhead_1, arrowhead_2, arrowhead_3, RED);
+        
+        DrawCircleLines(position.x, position.y, collisionradius, BLACK);
+
+        DrawCircleSector(position,250,facing_angle*RAD2DEG+30,facing_angle*RAD2DEG,15,Color{10,120,200,100});
+        DrawCircleSector(position,250,facing_angle*RAD2DEG,facing_angle*RAD2DEG-30,15,Color{200,10,120,100});
+    }
+
+
 
     };
